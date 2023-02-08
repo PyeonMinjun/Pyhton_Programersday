d9fc7883a163cae3a4e69208e9656bfa7f90dcd7
PyeonMinjun <alswns9810@gmail.com>
2023-02-08 19:02:59 +0900

    편민준 : 02/08

---
+++ BOJ_DAY/Baaaakingdog/0x04/BKD_linked_list_test.py
@@ -0,0 +1,12 @@
+class Jss:
+    def __init__(self):
+        self.name = input("이름 : ")
+        self.age = input("나이 : ")
+        print("Jss 클래스 선언!")
+    def show(self):
+        print("show 실행!")    
+    
+
+a = Jss()
+
+a.six()
