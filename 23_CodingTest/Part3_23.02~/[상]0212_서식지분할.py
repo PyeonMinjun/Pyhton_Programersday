631a245154e90841180f0febf50a9605a071162b
PyeonMinjun <alswns9810@gmail.com>
2023-02-13 15:15:10 +0900

    편민준 : 02/13

--- 23_CodingTest/Part3_23.02~/[상]0212_서식지분할.py
+++ 23_CodingTest/Part3_23.02~/[상]0212_서식지분할.py
@@ -1,37 +1,44 @@
-n = int(input())
-ns = input()
-nss = list(str(ns))
-res = [0] * 27
+# n = int(input())
+# ns = input()
+# nss = list(str(ns))
+# res = [0] * 27
 
-# 97 ~ 123
-# 1 26
-cnt = 0
-mid = False
+# # 97 ~ 123
+# # 1 26
+# cnt = 0
+# mid = False
 
-for i in nss:
-    o = ord(i) - 96
-    res[o] += 1
-    if res[o] >= 2:
-        mid = o
-        break
-    else:
-        cnt += 1
+# for i in nss:
+#     o = ord(i) - 96
+#     res[o] += 1
+#     if res[o] >= 2:
+#         mid = o
+#         break
+#     else:
+#         cnt += 1
         
-if mid == False:
-    print(cnt)
-    exit(0)
+# if mid == False:
+#     print(cnt)
+#     exit(0)
     
-mid = chr(mid+96)
-scr= ns.index(mid,ns.index(mid)+1)
-left = ns[:scr]
-right = ns[scr:]
+# mid = chr(mid+96)
+# scr= ns.index(mid,ns.index(mid)+1)
+# left = ns[:scr]
+# right = ns[scr:]
 
 
-arr2 = [0] * 27
-for i in right:
-    o2 = ord(i)-97
-    arr2[o2] = 1
+# arr2 = [0] * 27
+# for i in right:
+#     o2 = ord(i)-97
+#     arr2[o2] = 1
 
 
-maxPlace= (len(left) + arr2.count(1))
-print(maxPlace)
+# maxPlace= (len(left) + arr2.count(1))
+# print(maxPlace)
+
+n=int(input())
+arr=input()
+score=0
+for i in range(1,n):
+   score = max(score,len(set(arr[:i])) + len(set(arr[i:])))
+print(score)
