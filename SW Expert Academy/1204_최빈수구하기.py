4584cf4dfc58b67ce7ef58c738ac66a52abb190b
PyeonMinjun <alswns9810@gmail.com>
2023-04-04 09:47:06 +0900

    1204_최빈수구하기

---
+++ SW Expert Academy/1204_최빈수구하기.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+
+for cnt in range(n):
+    cnt += 1
+    a = int(input())
+    ns = list(map(int,input().split()))
+
+    maxo = -1
+    val = -1
+    arr = [0] * 1000
+
+    for i in ns:
+        arr[i] += 1
+        
+        if arr[i] > maxo:
+            maxo = arr[i]
+            val = i
+        elif arr[i] == maxo:
+            if val < i:
+                val = i
+               
+                    
+        
+        
+    
+    # if arr.count(max(arr)) > 1:
+    #      = max(arr)         
+
+    print("#{} {}" .format(cnt, val))
+    
