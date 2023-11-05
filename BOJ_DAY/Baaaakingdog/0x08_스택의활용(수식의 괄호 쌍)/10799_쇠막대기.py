136e33b0097f9375a537f0ac26f8895c76d93a95
PyeonMinjun <alswns9810@gmail.com>
2023-11-05 14:58:25 +0900

    11/4

--- BOJ_DAY/Baaaakingdog/0x08_스택의활용(수식의 괄호 쌍)/10799_쇠막대기.py
+++ BOJ_DAY/Baaaakingdog/0x08_스택의활용(수식의 괄호 쌍)/10799_쇠막대기.py
@@ -6,7 +6,7 @@ for i in range(len(s)):
     if s[i] == '(':
         stack.append(s[i])
     else:
-        if i > 0 and s[i - 1] == '(':  # 레이저일 경우
+        if i > 0 and s[i-1] == '(':  # 레이저일 경우
             stack.pop()  # 앞에서 막대라고 착각하고 스택에 s[i]를 넣었으므로 pop
             ans += len(stack)  # 막대의 개수만큼 ans에 추가
         else:  # 막대의 끝일 경우
